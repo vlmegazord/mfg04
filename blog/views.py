ced88a53a7bcb42ce291c7b4ecfad3b8cadf0fc7
Vassily <vasil.deville@gmail.com>
2017-07-16 10:25:23 -0400

    Basic html for the list of posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone as tz
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(date_published__lte=tz.now()).order_by('-date_published')
+    return render(request, 'blog/post_list.html', {'posts': posts})
