98930f158b7bb3bdebfa4af98b5b98cb3e8d106d
Vassily <vasil.deville@gmail.com>
2017-07-16 12:53:26 -0400

    Add view and template for the blog detail and the CSS elements

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone as tz
 from .models import Post
 
@@ -6,3 +6,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(date_published__lte=tz.now()).order_by('-date_published')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
